47eb6c1673433544c56a29861a5e83228787bc1d
xin-yamashita <koichiro.yamashita@xin.inc>
2024-06-21 15:33:56 +0900

    Jooin us ページ調整

---
+++ src/content/Story.js
@@ -0,0 +1,31 @@
+/**
+ * *
+ * JOIN-USで紹介用の記事一覧
+ */
+export const STORY_LIST = [
+  {
+    thumnail: 'https://images.wantedly.com/i/st1q1Qm?w=660&h=280',
+    title: 'イベントレポートまとめ - 最近のXIN -',
+    url: 'https://www.wantedly.com/companies/company_6613626/post_articles/882633',
+  },
+  {
+    thumnail: 'https://images.wantedly.com/i/st1q1Qm?w=660&h=280',
+    title: 'Figmaを導入した話 - 最近のXIN -',
+    url: 'https://www.wantedly.com/companies/company_6613626/post_articles/879712',
+  },
+  {
+    thumnail: 'https://images.wantedly.com/i/DCsFx34?w=660&h=280',
+    title: 'XINのヒストリー - XIN誕生までの歩みを知る -',
+    url: 'https://www.wantedly.com/companies/company_6613626/post_articles/873287',
+  },
+  {
+    thumnail: 'https://images.wantedly.com/i/j6sX2rD?w=660&h=280',
+    title: '【メンバーの声】XINの入社前後のギャップについて',
+    url: 'https://www.wantedly.com/companies/company_6613626/post_articles/547871',
+  },
+  {
+    thumnail: 'https://images.wantedly.com/i/st1q1Qm?w=660&h=280',
+    title: 'イベントレポートまとめ - 最近のXIN -',
+    url: 'https://www.wantedly.com/companies/company_6613626/post_articles/882633',
+  },
+];
